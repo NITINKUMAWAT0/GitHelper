7f6bcb0c1a8f70ef184e954c40770beddcabc834
Nitin Kumawat <nitin.kumawat.jaipur.in@gmail.com>
2025-04-15 02:44:26 +0530

    .

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -9,23 +9,39 @@ const Dashboard = () => {
   const currentProject = projects.find(project => project.id === projectId)
 
   return (
-    <div>      
+    <div>
       {currentProject?.githubUrl && (
-        <div className='flex items-center justify-center flex-wrap gap-y-4'>
+        <div className='flex items-center justify-between flex-wrap gap-y-4'>
+          {/* github link */}
           <div className='flex items-center w-fit rounded-md bg-primary px-4 py-3'>
-            <Github className='size-5 text-white'/>
+            <Github className='size-5 text-white' />
             <div className='ml-2'>
               <p className='text-sm font-medium text-white'>
                 This project is linked to{' '}
                 <Link href={currentProject.githubUrl} target="_blank" rel="noopener noreferrer" className='inline-flex items-center text-white/80 hover:underline'>
                   {currentProject.githubUrl}
-                  <ExternalLink className='ml-1 size-4'/>
+                  <ExternalLink className='ml-1 size-4' />
                 </Link>
               </p>
             </div>
           </div>
+
+          <div className="h-4"></div>
+
+          <div className='flex items-center gap-4'>
+            Team Members
+            Invite Button
+            Archive Button
+          </div>
         </div>
       )}
+
+      <div className="mt-4">
+        <div className='grid grid-cols-1 gap-4 sm:grid-cols-5'>
+          Ask Question Card
+          Meeting Card
+        </div>
+      </div>
     </div>
   )
 }
