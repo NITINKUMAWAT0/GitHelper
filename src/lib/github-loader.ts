4b1e147de312be3aea9a8d53d4f320f4105fc656
Nitin Kumawat <nitin.kumawat.jaipur.in@gmail.com>
2025-04-16 23:24:03 +0530

    .

--- src/lib/github-loader.ts
+++ src/lib/github-loader.ts
@@ -15,3 +15,23 @@ export const loadGithubRepo = async (githubUrl: string, githubToken?: string) =>
 }
 
 console.log(await loadGithubRepo("https://github.com/NITINKUMAWAT0/URL-SHORTENER-APPLICATION"));
+
+// Document {
+//     pageContent: '\n' +
+//       'const NotFound = () => {\n' +
+//       '    return (\n' +
+//       '      <div>\n' +
+//       '        <h1>404 not found</h1>\n' +
+//       '      </div>\n' +
+//       '    )\n' +
+//       '  }\n' +
+//       '  \n' +
+//       '  export default NotFound\n' +
+//       '  ',
+//     metadata: {
+//       source: 'url-shortener-frontend/src/Pages/NotFound/NotFound.jsx',
+//       repository: 'https://github.com/NITINKUMAWAT0/URL-SHORTENER-APPLICATION',
+//       branch: 'main'
+//     },
+//     id: undefined
+//   }
